885a3334042e49b840f539698665e8851b1180e9
juliet61008 <juliet61008@cj.net>
2025-11-27 14:34:28 +0900

    [juliet61008] Init

---
+++ src/main/java/com/jchat/jchatbe/JchatBeApplication.java
@@ -0,0 +1,13 @@
+package com.jchat.jchatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JchatBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JchatBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jchat/jchatbe/JchatBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchat.jchatbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JchatBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
