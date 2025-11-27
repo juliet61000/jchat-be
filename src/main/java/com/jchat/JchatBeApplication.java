5a76722581bd781f9ae9f9c907fb2c589d21b04b
juliet61008 <juliet61008@cj.net>
2025-11-27 15:30:19 +0900

    초기설정 및 테스트파일

--- src/main/java/com/jchat/JchatBeApplication.java
+++ src/main/java/com/jchat/JchatBeApplication.java
@@ -1,5 +1,6 @@
-package com.jchat.jchatbe;
+package com.jchat;
 
+import com.jchat.mem.controller.MemController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JchatBeApplication {
 
     public static void main(String[] args) {
+        MemController memController = new MemController();
+        memController.testMethod();
         SpringApplication.run(JchatBeApplication.class, args);
     }
 
